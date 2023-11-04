8938990fd59fa0b39a5ca2312ff9a51c7c5b91c5
Brandi Burnell <brandiburnell6@gmail.com>
2023-11-04 16:03:45 -0600

    filter by zipcode

--- index.js
+++ index.js
@@ -8,11 +8,30 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("#brewery-search-form").addEventListener("submit", e => {
         e.preventDefault();
         //console.log(e.target[0].value);
-        filterBreweries(e);
+        getBreweries(e);
     });
 });
 
-function getCOBreweryData() {
+// function getCOBreweryData() {
+//     let pageNum = 1;
+//     const coBreweryInfo = [];
+//     // loop through brewery data until list length is less than 50
+//     // loop through CO brewery data to develop list of all colorado breweries
+//     while(pageNum < 10) {
+//         fetch(`https://api.openbrewerydb.org/v1/breweries?by_state=colorado&page=${pageNum}&per_page=$50`)
+//         .then(res => res.json())
+//         .then(data => {
+//             // console.log(data.length);
+//             coBreweryInfo.push(...data);
+//         });
+//         pageNum++;
+//     }
+//     // console.log(coBreweryInfo);
+
+//     return coBreweryInfo;
+// }
+
+function getBreweries(e) {
     let pageNum = 1;
     const coBreweryInfo = [];
     // loop through brewery data until list length is less than 50
@@ -23,20 +42,21 @@ function getCOBreweryData() {
         .then(data => {
             // console.log(data.length);
             coBreweryInfo.push(...data);
+            if (coBreweryInfo.length > 400) {
+                filterBreweries(e, coBreweryInfo);
+            }
         });
         pageNum++;
     }
-    // console.log(coBreweryInfo);
-
-    return coBreweryInfo;
 }
 
-function filterBreweries(e) {
-
+function filterBreweries(e, breweries) {
     let city = e.target[0].value;
     let zipcode = e.target[1].value;
     let type = e.target[2].value;
-    const coBreweries = getCOBreweryData();
+    
+    // console.log(breweries);
+    // console.log(zipcode);
+    console.log(breweries.filter(brew => brew.postal_code.substring(0,5) === zipcode));
 
-    console.log(coBreweries);
 }
