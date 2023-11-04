620388f4cbbcea7b298a0ab8f823093dac7fcdec
Brandi Burnell <brandiburnell6@gmail.com>
2023-11-04 13:50:36 -0600

    gather all colorado brewery data

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 // 
-document.addEventListener("DOMContentLoaded", () => compileBreweryData());
+document.addEventListener("DOMContentLoaded", () => {
+
+    // compile list of all breweries in CO
+    getCOBreweryDataBreweryData()
+});
 
 
-function fetchData() {
-    fetch("https://api.openbrewerydb.org/v1/breweries")
+// function fetchData() {
+//     fetch("https://api.openbrewerydb.org/v1/breweries")
+//         .then(res => res.json())
+//         .then(data => console.log(data));
+// }
+
+function getCOBreweryData() {
+    
+    let pageNum = 1;
+    const coBreweryInfo = [];
+    // loop through brewery data until list length is less than 50
+    // loop through CO brewery data to develop list of all colorado breweries
+    while(pageNum < 10) {
+        fetch(`https://api.openbrewerydb.org/v1/breweries?by_state=colorado&page=${pageNum}&per_page=$50`)
         .then(res => res.json())
-        .then(data => console.log(data));
-}
+        .then(data => {
+            // console.log(data.length);
+            coBreweryInfo.push(...data);
+        });
+        pageNum++;
+    }
+    // console.log(coBreweryInfo);
 
-function compileBreweryData() {
-
-    fetch("https://api.openbrewerydb.org/v1/breweries?by_state=colorado")
-    .then(res => res.json())
-    .then(data => console.log(data));
+    return coBreweryInfo;
 }
