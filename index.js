a2ef2fe4d6412da786634d3818bdded842883918
Brandi Burnell <brandiburnell6@gmail.com>
2023-11-04 15:09:12 -0600

    create function to process user input

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // compile list of all breweries in CO
     // getCOBreweryDataBreweryData()
+
+    // Take user input and display breweries that meet search criteria
+    document.querySelector("#brewery-search-form").addEventListener("submit", e => {
+        e.preventDefault();
+        //console.log(e.target[0].value);
+        filterBreweries(e);
+    });
 });
 
 function getCOBreweryData() {
-    
     let pageNum = 1;
     const coBreweryInfo = [];
     // loop through brewery data until list length is less than 50
@@ -23,4 +29,14 @@ function getCOBreweryData() {
     // console.log(coBreweryInfo);
 
     return coBreweryInfo;
+}
+
+function filterBreweries(e) {
+
+    let city = e.target[0].value;
+    let zipcode = e.target[1].value;
+    let type = e.target[2].value;
+    const coBreweries = getCOBreweryData();
+
+    console.log(coBreweries);
 }
