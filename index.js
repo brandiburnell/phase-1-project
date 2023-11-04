d0c335120b2de28424feb24b955230bc12335dda
Brandi Burnell <brandiburnell6@gmail.com>
2023-11-04 12:57:30 -0600

    grabbed initial API data

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 // 
-document.addEventListener("DOMContentLoaded", () => console.log("we have connected :)"));
+document.addEventListener("DOMContentLoaded", () => compileBreweryData());
+
+
+function fetchData() {
+    fetch("https://api.openbrewerydb.org/v1/breweries")
+        .then(res => res.json())
+        .then(data => console.log(data));
+}
+
+function compileBreweryData() {
+
+    fetch("https://api.openbrewerydb.org/v1/breweries?by_state=colorado")
+    .then(res => res.json())
+    .then(data => console.log(data));
+}
