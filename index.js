5abd8528fbbcbc6b201b4d11cf1f4e06323fafcc
Brandi Burnell <brandiburnell6@gmail.com>
2023-11-09 10:27:27 -0700

    return to original loop structure

--- index.js
+++ index.js
@@ -41,21 +41,45 @@ function getBreweries(e) {
         existingBreweries.removeChild(existingBreweries.lastChild);
     }
 
-    let pageNum = 1;
+    let perPage = 50;
     const coBreweryInfo = [];
     // loop through brewery data until list length is less than 50
     // loop through CO brewery data to develop list of all colorado breweries
-    fetchData("https://api.openbrewerydb.org/v1/breweries?by_state=colorado&page=1&per_page=$50");
-}
-
-function fetchData(apiUrl) {
-    fetch(apiUrl)
+    while(perPage === 50) {
+        fetch(`https://api.openbrewerydb.org/v1/breweries?by_state=colorado&page=1&per_page=$${perPage}`)
         .then(res => res.json())
         .then(data => {
-            console.log(data);
-            console.log(data["next_page"]);
+            perPage = data.length;
+            coBreweryInfo.push(...data);
+            if (coBreweryInfo.length > 400) {
+                filterBreweries(e, coBreweryInfo);
+            }
         });
+    }
 }
+
+// function getBreweries(e) {
+//     // delete current content if it exists
+//     let existingBreweries = document.querySelector("#brewery-container");
+//     while (existingBreweries.firstChild) {
+//         existingBreweries.removeChild(existingBreweries.lastChild);
+//     }
+
+//     let pageNum = 1;
+//     const coBreweryInfo = [];
+//     // loop through brewery data until list length is less than 50
+//     // loop through CO brewery data to develop list of all colorado breweries
+//     fetchData("https://api.openbrewerydb.org/v1/breweries?by_state=colorado&page=1&per_page=$50");
+// }
+
+// function fetchData(apiUrl) {
+//     fetch(apiUrl)
+//         .then(res => res.json())
+//         .then(data => {
+//             console.log(data);
+//             console.log(data["next_page"]);
+//         });
+// }
 
 function filterBreweries(e, breweries) {
     let city = e.target[0].value.toLowerCase();
